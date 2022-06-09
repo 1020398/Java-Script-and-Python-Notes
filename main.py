4994212a868c5e757b206dd8c150e82f083cdc00
Siya Sharma <106357208+1020398@users.noreply.github.com>
2022-06-09 23:40:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+# Printing
+print("This Project is for PYTHON and JAVA SCRIPT for loops")
+
+# Making a variable
+python = random.randint(1,100)
+
+# Conditionals
+if python > 58:
+  print(python)
+
+# Loops
+list = [1,2,3,4,5]
+for yin in list:
+  print(yin)
+
+for yang in range(len(list)):
+  print(yang)
---
+++ script.js
@@ -0,0 +1,15 @@
+// Print to console
+console.log("This Project is for PYTHON and JAVA SCRIPT")
+
+// Make a Variable
+var java = 78
+
+// Conditionals 
+if(java > 20){
+  console.log(java)
+}
+
+// Loops {can separate parameters with semi-colons}
+for(var yin = 1, yin < 6, yin++){  
+  console.log(yin)
+}
